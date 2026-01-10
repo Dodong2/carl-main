ad500b2173548aa406f641a47a7b8ecfa4da4a05
Dodong2 <carlseighartaliode@gmail.com>
2026-01-10 22:47:57 +0800

    adjust UI

--- vite.config.ts
+++ vite.config.ts
@@ -2,30 +2,34 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
+import type { VitePWAOptions } from 'vite-plugin-pwa'
+
+const PwaManifest: Partial<VitePWAOptions> = {
+  registerType: 'autoUpdate',
+  includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+  manifest: {
+    name: 'Castar Web Port',
+    short_name: 'Castar Port',
+    description: 'Castar Portfolio App',
+    icons: [
+      {
+        src: 'sword-chatgpt-removeBG.png',
+        sizes: '192x192',
+        type: 'image/png'
+      },
+    ],
+    theme_color: '	#303030',
+    background_color: '#303030',
+    orientation: 'portrait',
+  },
+}
+
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/carl-main/",
   plugins: [react(),
-  VitePWA({
-    registerType: 'autoUpdate',
-    includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
-    manifest: {
-      name: 'Castar',
-      short_name: 'Castar Web App',
-      description: 'Castar Portfolio App',
-      icons: [
-        {
-          src: 'dong.png',
-          sizes: '192x192',
-          type: 'image/png'
-        },
-      ],
-      theme_color: '	#303030',
-      background_color: '#303030',
-      orientation: 'portrait',
-    }
-  }),
+  VitePWA(PwaManifest),
   tailwindcss()],
   build: {
     chunkSizeWarningLimit: 1000
