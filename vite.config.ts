58c18caf4b8a175147fced7bdada7bc676889e7a
Dodong2 <carlseighartaliode@gmail.com>
2026-01-10 21:41:01 +0800

    adjust UI added PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/carl-main/",
-  plugins: [react(), tailwindcss()],
+  plugins: [react(),
+  VitePWA({
+    registerType: 'autoUpdate',
+    includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+    manifest: {
+      name: 'Castar',
+      short_name: 'Castar Web App',
+      description: 'Castar Portfolio App',
+      icons: [
+        {
+          src: 'dong.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+      ],
+      theme_color: '	#303030',
+      background_color: '	#303030',
+      orientation: 'portrait',
+    }
+  }),
+  tailwindcss()],
   build: {
     chunkSizeWarningLimit: 1000
-  }
+  },
+
 })
