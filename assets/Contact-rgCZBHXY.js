121504aaf4ae6e100d67d104631cfb64a771aae6
Dodong2 <carlseighartaliode@gmail.com>
2026-01-10 20:37:22 +0800

    Updates

--- assets/Contact-rgCZBHXY.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-BNpi_sPj.js";const s=()=>t.jsx("section",{id:"contacts",className:"min-h-screen p-10 border-2 border-blue-700",children:t.jsx("h1",{className:"text-white",children:"Contacts"})});export{s as default};
