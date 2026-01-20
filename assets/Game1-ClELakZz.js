300abb05440f62b21f39549f60bcb1e2f37ead05
Dodong2 <carlseighartaliode@gmail.com>
2026-01-20 22:04:23 +0800

    Updates

--- assets/Game1-ClELakZz.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-DwIJRhBb.js";const s=()=>r.jsx("div",{className:"bg-sky-400 border-4 border-black p-12",children:"Game1"});export{s as default};
--- assets/Game2-6Yvo5HX3.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-DwIJRhBb.js";const s=()=>r.jsx("div",{className:"bg-sky-400 border-4 border-black p-12",children:"Game2"});export{s as default};
